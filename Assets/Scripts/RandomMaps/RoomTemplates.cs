30d47408cd3e7c445eb95f4437a787731ae35106
hayden504078 <qaz504078@gmail.com>
2019-12-06 02:58:43 +0800

    增加BossRom3D與傳點

--- Assets/Scripts/RandomMaps/RoomTemplates.cs
+++ Assets/Scripts/RandomMaps/RoomTemplates.cs
@@ -15,14 +15,16 @@ public class RoomTemplates : MonoBehaviour {
 
 	public float waitTime;
 	private bool spawnedBoss;
-	public GameObject boss;
+	private int rand; // 隨機Boss房
+	public GameObject[] bossRooms;
 
 	void Update(){
 
 		if(waitTime <= 0 && spawnedBoss == false){
 			for (int i = 0; i < rooms.Count; i++) {
 				if(i == rooms.Count-1){
-					Instantiate(boss, rooms[i].transform.position + new Vector3(10,-10,0), Quaternion.identity);
+					rand = Random.Range (0, bossRooms.Length);
+					Instantiate(bossRooms[rand], rooms[i].transform.position + new Vector3(10,-10,0), Quaternion.identity);
 					spawnedBoss = true;
 				}
 			}
