12e32cba0cc3a678aa409cf9255697bcdbe4e029
MBHAO <bill55171@gmail.com>
2019-12-06 20:35:53 +0800

    (合併版本後)武器更新, 怪物子彈&玩家子彈更新

--- Assets/Scripts/HurtPlayer.cs
+++ Assets/Scripts/HurtPlayer.cs
@@ -9,6 +9,7 @@ public class HurtPlayer : MonoBehaviour {
     public int damageToGive;                // 武器攻擊力
     private int currentDamage;              // 總攻擊力
     public GameObject damageNumber;         // 顯示傷害值效果
+    public bool destroyOnCollision = false;         // 使自身碰撞後消失
 
     private PlayerStats thePS;
 
@@ -21,13 +22,13 @@ public class HurtPlayer : MonoBehaviour {
 	void Update () {
 	}
 
-
     void OnCollisionEnter2D(Collision2D other)
     {
+
         if (other.gameObject.tag == "Player")
         {
-            currentDamage = damageToGive - thePS.currentDefence;
-            if (currentDamage < 0)
+            currentDamage = damageToGive - thePS.currentDefence; 
+            if ( currentDamage < 0)
             {
                 currentDamage = 0;
             }
@@ -37,25 +38,11 @@ public class HurtPlayer : MonoBehaviour {
             // 建立新的物件( Object 物件, Vector3 位置, Quaternion 旋轉 ) ; rotation有四格, Euler函數可以變成只給XYZ ( 我也不懂
             var clone = (GameObject)Instantiate(damageNumber, other.transform.position, Quaternion.Euler(Vector3.zero));
             clone.GetComponent<FloatingNumbers>().damageNumber = currentDamage;         // 給定傷害值
-        }
 
-    }
-
-    void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.gameObject.tag == "Player")
-        {
-            currentDamage = damageToGive - thePS.currentDefence;
-            if (currentDamage < 0)
+            if (destroyOnCollision)
             {
-                currentDamage = 0;
+                Destroy(transform.gameObject);
             }
-
-            other.gameObject.GetComponent<PlayerHealthManager>().HurtPlayer(currentDamage);
-
-            // 建立新的物件( Object 物件, Vector3 位置, Quaternion 旋轉 ) ; rotation有四格, Euler函數可以變成只給XYZ ( 我也不懂
-            var clone = (GameObject)Instantiate(damageNumber, other.transform.position, Quaternion.Euler(Vector3.zero));
-            clone.GetComponent<FloatingNumbers>().damageNumber = currentDamage;         // 給定傷害值
         }
 
     }
