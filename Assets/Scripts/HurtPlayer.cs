7f76e0a336376de7f74fb7e45f7955e932f27f20
MBHAO <bill55171@gmail.com>
2019-12-06 16:03:14 +0800

    玩家武器更新&HurtPlayer更新

--- Assets/Scripts/HurtPlayer.cs
+++ Assets/Scripts/HurtPlayer.cs
@@ -21,12 +21,32 @@ public class HurtPlayer : MonoBehaviour {
 	void Update () {
 	}
 
+
     void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.tag == "Player")
         {
-            currentDamage = damageToGive - thePS.currentDefence; 
-            if ( currentDamage < 0)
+            currentDamage = damageToGive - thePS.currentDefence;
+            if (currentDamage < 0)
+            {
+                currentDamage = 0;
+            }
+
+            other.gameObject.GetComponent<PlayerHealthManager>().HurtPlayer(currentDamage);
+
+            // 建立新的物件( Object 物件, Vector3 位置, Quaternion 旋轉 ) ; rotation有四格, Euler函數可以變成只給XYZ ( 我也不懂
+            var clone = (GameObject)Instantiate(damageNumber, other.transform.position, Quaternion.Euler(Vector3.zero));
+            clone.GetComponent<FloatingNumbers>().damageNumber = currentDamage;         // 給定傷害值
+        }
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            currentDamage = damageToGive - thePS.currentDefence;
+            if (currentDamage < 0)
             {
                 currentDamage = 0;
             }
