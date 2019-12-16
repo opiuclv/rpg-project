662678965293c20463fafef91a210d92e3e1d014
Chiu Zheng Hao <opiuclv@gmail.com>
2019-12-16 18:35:50 +0800

    新增更多劇情

---
+++ Assets/Scripts/MS_Part7_Holder.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MS_Part7_Holder : MonoBehaviour
+{
+    private Main_Story_Part7 theMS7;
+
+    // Use this for initialization
+    void Start()
+    {
+        theMS7 = FindObjectOfType<Main_Story_Part7>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            theMS7.ShowDialogue();
+            Destroy(gameObject); // 劇情已經觸發過一次就不要了
+        }
+    }
+}
