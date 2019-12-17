c128d6419737c60507ebbc6b22ac9768b22c77d7
hayden504078 <qaz504078@gmail.com>
2019-12-17 12:51:06 +0800

    .

---
+++ Assets/Scripts/monster/MonsterHpUi.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MonsterHpUi : MonoBehaviour
+{
+	
+	public Text HPText;
+	public EnemyHealthManager Monsterhealth;
+
+    // Update is called once per frame
+    void Update()
+    {
+		HPText.text = Monsterhealth.CurrentHealth + "/" + Monsterhealth.MaxHealth;
+    }
+}
